cf6b5f6e84b0719902d4c2be59ead0e45c11ac58
Hao <haodngn@gmail.com>
2021-10-20 15:40:49 +0700

    add log out and fix login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:it_support/screens/auth_screen/login_screen.dart';
 
 Future<void> main() async {
@@ -11,7 +12,7 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
