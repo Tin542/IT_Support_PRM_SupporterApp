e56930ac36f6d6b3c6aac2284a7eca0258d23cc7
Hao <haodngn@gmail.com>
2021-10-27 15:18:38 +0700

    add reset password

--- lib/screens/auth_screen/forgot_password_screen.dart
+++ lib/screens/auth_screen/forgot_password_screen.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:it_support/screens/auth_screen/login_screen.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
+  final TextEditingController emailTextEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,13 +17,6 @@ class ForgotPasswordScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              // Padding(
-              //   padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-              //   child: Container(
-              //       width: 380,
-              //       height: 300,
-              //       child: Image.asset('assets/images/logo.jpg')),
-              // ),
               Padding(
                 padding: const EdgeInsets.fromLTRB(0, 170, 0, 20),
                 child: Text(
@@ -33,39 +30,13 @@ class ForgotPasswordScreen extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.fromLTRB(0, 0, 0, 28),
                 child: TextField(
+                  controller:emailTextEditingController,
                   style: TextStyle(fontSize: 18, color: Colors.black),
                   decoration: InputDecoration(
                       labelText: "EMAIL",
                       labelStyle:
                           TextStyle(color: Color(0xff888888), fontSize: 15)),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-                child: TextField(
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: "MẬT KHẨU MỚI",
-                      labelStyle:
-                          TextStyle(color: Color(0xff888888), fontSize: 15)),
-                ),
-              ),
-              Stack(
-                alignment: AlignmentDirectional.centerEnd,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-                    child: TextField(
-                      style: TextStyle(fontSize: 18, color: Colors.black),
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          labelText: "XÁC NHẬN MẬT KHẨU",
-                          labelStyle: TextStyle(
-                              color: Color(0xff888888), fontSize: 15)),
-                    ),
-                  ),
-                ],
               ),
               Padding(
                 padding: const EdgeInsets.fromLTRB(0, 80, 0, 0),
@@ -76,7 +47,16 @@ class ForgotPasswordScreen extends StatelessWidget {
                     color: Colors.blue,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.all(Radius.circular(8))),
-                    onPressed: onSignInClicked,
+                    onPressed: () {
+                      if (!emailTextEditingController.text.contains("@")) {
+                        displayToastMessage("Email không hợp lệ", context);
+                      } else if (emailTextEditingController.text.isEmpty) {
+                        displayToastMessage(
+                            "Vui lòng nhập email.", context);
+                      } else {
+                        resetPassWord(context);
+                      }
+                    },
                     child: Text(
                       "LẤY LẠI MẬT KHẨU",
                       style: TextStyle(color: Colors.white, fontSize: 16),
@@ -91,5 +71,17 @@ class ForgotPasswordScreen extends StatelessWidget {
     );
   }
 
-  void onSignInClicked() {}
+  final FirebaseAuth _firebaseAuth =
+      FirebaseAuth.instance;
+
+  void resetPassWord(BuildContext context) async {
+    _firebaseAuth.sendPasswordResetEmail(email: emailTextEditingController.text);
+    displayToastMessage("Mail để lấy lại mật khẩu đã được gửi.", context);
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
+
+  displayToastMessage(String message, BuildContext context) {
+    Fluttertoast.showToast(msg: message);
+  }
 }
